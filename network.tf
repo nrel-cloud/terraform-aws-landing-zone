871b9d04fada761ea8a02500da14d24b0a653d62
nrelmmoore <Michael.Moore@nrel.gov>
2024-07-10 00:19:32 -0600

    Update

--- network.tf
+++ network.tf
@@ -1,17 +1,42 @@
-/*data "aws_vpc" "vpc01" {
-  provider = aws.shared_account_location01
-  filter {
-    name   = "tag:Name"
-    values = ["*general*"]  
-  }
+locals {
+  shared_subnet_filter_location01 = [for name in var.shared_subnets_names : "*${name}*" if can(regex(".*${replace(var.location01, "-", "")}.*", name))]
+  shared_subnet_filter_location02 = [for name in var.shared_subnets_names : "*${name}*" if can(regex(".*${replace(var.location02, "-", "")}.*", name))]
+  shared_subnet_filter_location03 = [for name in var.shared_subnets_names : "*${name}*" if can(regex(".*${replace(var.location03, "-", "")}.*", name))]
+  shared_subnet_filter_location04 = [for name in var.shared_subnets_names : "*${name}*" if can(regex(".*${replace(var.location04, "-", "")}.*", name))]
 }
-*/
 
 data "aws_subnet" "subnets_shared_location01" {
   provider = aws.shared_account_location01
-  for_each = var.shared_subnets_names
+  count    = length(local.shared_subnet_filter_location01)
   filter {
     name   = "tag:Name"
-    values = [each.value]
+    values = toset(local.shared_subnet_filter_location01)
+  }
+}
+
+data "aws_subnet" "subnets_shared_location02" {
+  provider = aws.shared_account_location02
+  count    = length(local.shared_subnet_filter_location02)
+  filter {
+    name   = "tag:Name"
+    values = toset(local.shared_subnet_filter_location02)
+  }
+}
+
+data "aws_subnet" "subnets_shared_location03" {
+  provider = aws.shared_account_location03
+  count    = length(local.shared_subnet_filter_location03)
+  filter {
+    name   = "tag:Name"
+    values = toset(local.shared_subnet_filter_location03)
+  }
+}
+
+data "aws_subnet" "subnets_shared_location04" {
+  provider = aws.shared_account_location04
+  count    = length(local.shared_subnet_filter_location04)
+  filter {
+    name   = "tag:Name"
+    values = toset(local.shared_subnet_filter_location04)
   }
 }
--- resourceshare.tf
+++ resourceshare.tf
@@ -1,23 +1,103 @@
-
-resource "aws_ram_resource_share" "ramshare01" {
+#share subnets in location01
+resource "aws_ram_resource_share" "ram_share01_location01" {
   provider = aws.shared_account_location01
   name = join("-",["ramshare",var.shared_account_id,"to",var.spoke_account_id])
   allow_external_principals = false
 }
 
 
-resource "aws_ram_resource_association" "ramshareassociation01" {
+resource "aws_ram_resource_association" "ram_share_association01_location01" {
   provider           = aws.shared_account_location01
-  for_each           = data.aws_subnet.subnets_shared_location01
-  resource_arn       = each.value["arn"]
-  resource_share_arn = aws_ram_resource_share.ramshare01.arn
+  count              = length(data.aws_subnet.subnets_shared_location01)
+  resource_arn       = data.aws_subnet.subnets_shared_location01[count.index].arn
+  resource_share_arn = aws_ram_resource_share.ram_share01_location01.arn
 
   depends_on = [ data.aws_subnet.subnets_shared_location01 ]
 }
 
-resource "aws_ram_principal_association" "ramprincipalassociation01" {
-  provider = aws.shared_account_location01
+resource "aws_ram_principal_association" "ram_principal_association01_location01" {
+  provider            = aws.shared_account_location01
   principal          = var.spoke_account_id
-  resource_share_arn = aws_ram_resource_share.ramshare01.arn
+  resource_share_arn = aws_ram_resource_share.ram_share01_location01.arn
 }
+
+
+
+#share subnets in location02
+
+resource "aws_ram_resource_share" "ram_share01_location02" {
+  provider = aws.shared_account_location02
+  name = join("-",["ramshare",var.shared_account_id,"to",var.spoke_account_id])
+  allow_external_principals = false
+}
+
+
+resource "aws_ram_resource_association" "ram_share_association01_location02" {
+  provider           = aws.shared_account_location02
+  count              = length(data.aws_subnet.subnets_shared_location02)
+  resource_arn       = data.aws_subnet.subnets_shared_location02[count.index].arn
+  resource_share_arn = aws_ram_resource_share.ram_share01_location02.arn
+
+  depends_on = [ data.aws_subnet.subnets_shared_location02 ]
+}
+
+resource "aws_ram_principal_association" "ram_principal_association01_location02" {
+  provider            = aws.shared_account_location02
+  principal          = var.spoke_account_id
+  resource_share_arn = aws_ram_resource_share.ram_share01_location02.arn
+}
+
+
+
+#share subnets in location03
+
+resource "aws_ram_resource_share" "ram_share01_location03" {
+  provider = aws.shared_account_location03
+  name = join("-",["ramshare",var.shared_account_id,"to",var.spoke_account_id])
+  allow_external_principals = false
+}
+
+
+resource "aws_ram_resource_association" "ram_share_association01_location03" {
+  provider           = aws.shared_account_location03
+  count              = length(data.aws_subnet.subnets_shared_location03)
+  resource_arn       = data.aws_subnet.subnets_shared_location03[count.index].arn
+  resource_share_arn = aws_ram_resource_share.ram_share01_location03.arn
+
+  depends_on = [ data.aws_subnet.subnets_shared_location03 ]
+}
+
+resource "aws_ram_principal_association" "ram_principal_association01_location03" {
+  provider            = aws.shared_account_location03
+  principal          = var.spoke_account_id
+  resource_share_arn = aws_ram_resource_share.ram_share01_location03.arn
+}
+
+
+
+#share subnets in location04
+
+resource "aws_ram_resource_share" "ram_share01_location04" {
+  provider = aws.shared_account_location04
+  name = join("-",["ramshare",var.shared_account_id,"to",var.spoke_account_id])
+  allow_external_principals = false
+}
+
+
+resource "aws_ram_resource_association" "ram_share_association01_location04" {
+  provider           = aws.shared_account_location04
+  count              = length(data.aws_subnet.subnets_shared_location04)
+  resource_arn       = data.aws_subnet.subnets_shared_location04[count.index].arn
+  resource_share_arn = aws_ram_resource_share.ram_share01_location04.arn
+
+  depends_on = [ data.aws_subnet.subnets_shared_location04 ]
+}
+
+resource "aws_ram_principal_association" "ram_principal_association01_location04" {
+  provider            = aws.shared_account_location04
+  principal          = var.spoke_account_id
+  resource_share_arn = aws_ram_resource_share.ram_share01_location04.arn
+}
+
+
 

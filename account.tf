feb226054a055a6484eca34570d44a75167575f0
nrelmmoore <Michael.Moore@nrel.gov>
2024-07-10 00:22:55 -0600

    Update account.tf

--- account.tf
+++ account.tf
@@ -9,6 +9,39 @@ provider "aws" {
               }
 
 }
+provider "aws" {
+  alias = "shared_account_location02"
+  region = var.location02  # Modify this to your preferred region
+
+    assume_role {
+    # The role ARN within Account B to AssumeRole into.
+    role_arn    = join(":",["arn:aws:iam:",var.shared_account_id,"role/NREL-TerraformRole"])
+
+              }
+
+}
+provider "aws" {
+  alias = "shared_account_location03"
+  region = var.location03  # Modify this to your preferred region
+
+    assume_role {
+    # The role ARN within Account B to AssumeRole into.
+    role_arn    = join(":",["arn:aws:iam:",var.shared_account_id,"role/NREL-TerraformRole"])
+
+              }
+
+}
+provider "aws" {
+  alias = "shared_account_location04"
+  region = var.location04  # Modify this to your preferred region
+
+    assume_role {
+    # The role ARN within Account B to AssumeRole into.
+    role_arn    = join(":",["arn:aws:iam:",var.shared_account_id,"role/NREL-TerraformRole"])
+
+              }
+
+}
 
 provider "aws" {
   alias = "spoke_account_location01"
